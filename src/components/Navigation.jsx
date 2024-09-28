010d7a5fd6316ec5b2f350a9723b5fa1f68038a0
Nebuchadnezzar <68788407+VikXNebu@users.noreply.github.com>
2024-09-28 20:46:50 +0800

    Create Navigation.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <ul className="flex space-x-4">
+        <li><Link to="/" className="hover:text-blue-300">Home</Link></li>
+        <li><Link to="/explore-strategy" className="hover:text-blue-300">Explore Strategy</Link></li>
+        <li><Link to="/travel-tools" className="hover:text-blue-300">Travel Tools</Link></li>
+        <li><Link to="/sports-hospitality" className="hover:text-blue-300">Sports Hospitality</Link></li>
+        <li><Link to="/online-education" className="hover:text-blue-300">Online Education</Link></li>
+        <li><Link to="/entrepreneurship" className="hover:text-blue-300">Entrepreneurship</Link></li>
+        <li><Link to="/newsletter-signup" className="hover:text-blue-300">Newsletter Signup</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
