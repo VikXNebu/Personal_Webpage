51dddffbc952bc4a5055f498c3903da52323d3a2
Nebuchadnezzar <68788407+VikXNebu@users.noreply.github.com>
2024-09-29 10:34:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Index from "./pages/Index";
 import ExploreStrategy from "./pages/ExploreStrategy";
 import TravelTools from "./pages/TravelTools";
